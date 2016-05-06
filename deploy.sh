cf15dec956a5990c13eb16d30752e9cbe7afa7e5
Keegan Farley <keegomyneego@live.com>
2016-05-06 16:58:20 -0700

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REPO_DIR=`git rev-parse --show-toplevel`
+
+promptContinue() {
+	printf "Would like to continue? (Y/N) "
+
+	read continue
+
+	repliedY=`echo "$continue" | grep -iE "^(y|yes)$" 2> /dev/null`
+	repliedN=`echo "$continue" | grep -iE "^(n|no)$" 2> /dev/null`
+
+	if [ -n "$repliedY" ] ; then
+		echo "Continuing..."
+	elif [ -n "$repliedN" ] ; then
+		echo "Deployment canceled"
+		exit 0
+	else
+		echo "Response unclear"
+		promptContinue
+	fi
+}
+
+echo "D E P L O Y   S P A R K   W E B S I T E"
+echo "This will upload your local files to the webserver, overwriting all existing content."
+
+promptContinue
+
+echo "Uploading files to webserver..."
+scp -rp "$REPO_DIR" ksf7@vergil.u.washington.edu:./public_html
+
+echo "Upload complete"
+exit 1
